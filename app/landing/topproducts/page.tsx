b6195d4a00c84b5e8d6c1fdbe78421350c626870
nwachira <nwachira@unganishanetworks.com>
2024-07-30 13:12:32 +0300

    fixed top products

--- app/landing/topproducts/page.tsx
+++ app/landing/topproducts/page.tsx
@@ -28,7 +28,7 @@ const ProductsData = [
       "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
   },
 ];
-const TopProducts = ({ handleOrderPopup }) => {
+const TopProducts = ({ }) => {
   return (
     <div>
       <div className="container">
@@ -76,7 +76,7 @@ const TopProducts = ({ handleOrderPopup }) => {
                 </p>
                 <button
                   className="bg-primary hover:scale-105 duration-300 text-white py-1 px-4 rounded-full mt-4 group-hover:bg-white group-hover:text-primary"
-                  onClick={handleOrderPopup}
+                 
                 >
                   Order Now
                 </button>
