616f67d0b2a2d5b3112be9a4dbc9f407ed003d4e
nwachira <nwachira@unganishanetworks.com>
2024-07-30 12:54:48 +0300

    removed testimonials

--- app/landing/topproducts/page.tsx
+++ app/landing/topproducts/page.tsx
@@ -47,8 +47,9 @@ const TopProducts = ({ handleOrderPopup }) => {
         </div>
         {/* Body section */}
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-20 md:gap-5 place-items-center">
-          {ProductsData.map((data) => (
+          {ProductsData.map((data, index) => ( // Add index as the key
             <div
+              key={index} // Add key here
               data-aos="zoom-in"
               className="rounded-2xl bg-white dark:bg-gray-800 hover:bg-black/80 dark:hover:bg-primary hover:text-white relative shadow-xl duration-300 group max-w-[300px]"
             >
